80c795e277007fcc8c6430e60affa5250e1ab9ad
Gallahar <david.kushnir.d@gmail.com>
2023-01-10 23:50:04 +0300

    Transitions/animations

---
+++ src/Lessons/Lesson10/index.tsx
@@ -0,0 +1,36 @@
+import "./index.scss";
+
+export const Lesson10 = () => {
+  return (
+    <div className={"lesson10"}>
+      <header>
+        <div className="container">
+          <img
+            src="public/lesson10/ne.png"
+            className="logo"
+            width="101"
+            height="101"
+            alt="Логотип"
+          />
+          <h1>Трансформации, переходы и анимация CSS</h1>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Главная</a>
+              </li>
+              <li>
+                <a href="#">Галерея</a>
+              </li>
+              <li>
+                <a href="#">История</a>
+              </li>
+              <li>
+                <a href="#">О нас</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+};
